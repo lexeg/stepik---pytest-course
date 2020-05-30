5cea5061836d04108cf0134f259a1d10d97a0395
lexeg <klim2041@yandex.ru>
2020-05-30 10:28:02 +0500

    add assert and timer to test

--- test_items.py
+++ test_items.py
@@ -1,5 +1,9 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_catalogue_should_see_add_to_basket_button_pass(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn-add-to-basket")
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button, "basket not found"
+    time.sleep(30)
